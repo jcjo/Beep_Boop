1e4dc8e73dfa475fba61dc45f848ad28a7451d04
Jacey Tupa <jcjomn@icloud.com>
2018-10-26 10:44:54 -0700

    Add loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,44 @@
+//
+// $(document).ready(function() {
+//   $("form#beepForm").submit(function(event) {
+//     var userNumber = $("input#numberInput").val();
+//     event.preventDefault();
+//     $("#beepPhrase").append(userNumber + " ");
+//   });
+// });
 
-
+// $(document).ready(function() {
+//   $("form#beepForm").submit(function(event) {
+//     event.preventDefault();
+//     var userNumber = $("input#numberInput").val();
+//
+//     if (userNumber === 1) {
+//       $("#beepPhrase").append(userNumber + "!");
+//     } else if (userNumber != 1) {
+//         $("#beepPhrase").append(userNumber + " ");
+//       }
+//   });
+// });
 
 
 $(document).ready(function() {
   $("form#beepForm").submit(function(event) {
-    var userNumber = $("input#numberInput").val();
     event.preventDefault();
-    $("#beepPhrase").append(userNumber + " ");
-    //
-    // if (userNumber === "1") {
-    //   $("#responseString").push(userNumber);
-    // } else {
-    //   alert ("Add number");
-    // }
+    function boopify (userNumber) {
+    var userNumber = $("input#numberInput").val();
+    var userNumberArray = userNumber.split(" ");
 
-
-
-    // function showNumber(userNumber) {
-    //   return userNumber;
-    // }
-
-  });
+    for (var i = 0; i <= userNumber.length; i ++) {
+      if (userNumberArray[i] === 0) {
+        $("#beepPhrase").append("Beep!");
+      } else if (userNumberArray[i] != 0) {
+         $("#beepPhrase").append(userNumberArray);
+        }
+      }
+    // } return "#beepPhrase";
+    }
+  })
 });
+
+
+$(document).read(function())
